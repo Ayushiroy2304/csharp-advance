a06e6eb961be7c1c3e6ec396f8925ad5a6749659
Kumari Ayushi <118763557+Ayushiroy2304@users.noreply.github.com>
2024-03-20 16:08:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encaps
+{
+    class Account()
+    {
+      private int Accountbalance =1000;     //declare it private
+    //     public void setBalance(int amount)
+    //     {
+    //         Accountbalance = amount;
+    //     }
+    //     public void getBalance()
+    //     {
+    //         System.Console.WriteLine("YOUR ACCOUNT BALANCE IS "+ Accountbalance);
+    //     }
+    // }
+    public int balance
+    {
+        set 
+        {
+                   
+        }
+
+    }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myaccount = new Account();
+            myaccount.setBalance(500000);  //the value of the account balance is being changend ; that is why encapsulation is used
+            myaccount.getBalance();
+            System.Console.ReadLine();
+        }
+    }
+}
